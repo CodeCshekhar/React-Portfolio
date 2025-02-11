42d8724e1f7628dccb51b1be80f0bae74cbfe6e1
Chandrashekharwagh <waghchandu2002@gmail.com>
2025-02-11 20:16:23 +0530

    major changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,18 @@
 function Contact() {
     return (
       <section className="container mx-auto p-10">
-        <div class="section-content">
-        <h2 className="text-3xl font-bold">Contact Me</h2>
-        <div class="wrapper">
-          <a href="https://www.linkedin.com/in/chandrashekhar-wagh-240197330/" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
-          <a href="https://github.com/CodeCshekhar" class="icon"><i class="fa-brands fa-github"></i></a>
-          <a href="https://www.instagram.com/chandrashekhar_wagh_23/" class="icon"><i class="fa-brands fa-instagram"></i></a>
-          <a href="https://x.com/ChanduWagh23" class="icon"><i class="fa-brands fa-x-twitter"></i></a>
+        <div className="contact-card">
+
+          <h2 className="text-3xl font-bold">Contact Me</h2>
+          <div className="wrapper">
+            <a href="https://www.linkedin.com/in/chandrashekhar-wagh-240197330/" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+            <a href="https://github.com/CodeCshekhar" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="https://www.instagram.com/chandrashekhar_wagh_23/" className="icon"><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://x.com/ChanduWagh23" className="icon"><i className="fa-brands fa-x-twitter"></i></a>
           </div>
-          </div>
+        </div>
       </section>
     );
-  }
-  
-  export default Contact;
+}
+
+export default Contact;
