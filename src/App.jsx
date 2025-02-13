e97676c163609f83952d7a9f2a4422f24c5784af
Chandrashekharwagh <waghchandu2002@gmail.com>
2025-02-13 20:06:19 +0530

    major changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Education from "./components/Education";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
 import "./styles/global.css";
+import "./styles/mobile.css";
 
 function App() {
   return (
